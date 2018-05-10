7a7b58fd2542b7bc28c7ab43201ca38b8dfcd362
Jonah Wooten <jonahwooten@UltraMagnus.local>
2018-05-10 17:31:08 -0400

    random bomb

--- src/midtermproject/Grid.java
+++ src/midtermproject/Grid.java
@@ -7,38 +7,35 @@ public class Grid {
 
 			{ { false, false, false }, { false, true, true }, { true, true, false } };
 
-	/*
-	 * public boolean [][] random; public int mineCount;
-	 * 
-	 * 
-	 * 
-	 * public boolean[][] getRandom() { return random; }
-	 * 
-	 * 
-	 * 
-	 * public void setRandom(boolean[][] random) { this.random = random; }
-	 * 
-	 * 
-	 * 
-	 * public int getMineCount() { return mineCount; }
-	 * 
-	 * 
-	 * 
-	 * public void setMineCount(int mineCount) { this.mineCount = mineCount; }
-	 * 
-	 * 
-	 * 
-	 * public Grid(boolean[][] random, int mineCount) { super(); this.random =
-	 * random; this.mineCount = mineCount;
-	 * 
-	 * 
-	 * } public static Grid setBombs(Grid random){
-	 * 
-	 * return random; }
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
+	public boolean[][] random;
+	public int mineCount;
+
+	public boolean[][] getRandom() {
+		return random;
+	}
+
+	public void setRandom(boolean[][] random) {
+		this.random = random;
+	}
+
+	public int getMineCount() {
+		return mineCount;
+	}
+
+	public void setMineCount(int mineCount) {
+		this.mineCount = mineCount;
+	}
+
+	public Grid(boolean[][] random, int mineCount) {
+		super();
+		this.random = random;
+		this.mineCount = mineCount;
+
+	}
+
+	public static Grid setBombs(Grid random) {
+
+		return random;
+	}
+
 }
