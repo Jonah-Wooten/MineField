531807dfd33c7060df35a9aeaaa42bf55d04e09f
Jonah Wooten <jonahwooten@UltraMagnus.local>
2018-05-10 15:46:00 -0400

    secret grid

---
+++ src/midtermproject/Grid.java
@@ -0,0 +1,16 @@
+package midtermproject;
+
+public class Grid {
+	
+	
+	
+	int arr[][] sweep = new int[3][3];
+	sweep [0] = new int[10];
+	sweep 0 = new int [][];
+
+	
+	for(int i = 0; i < rendered.length; i++) {
+    for(int j = 0; j < rendered[i].length; j++)
+        rendered[i][j] = false;
+	}
+}
