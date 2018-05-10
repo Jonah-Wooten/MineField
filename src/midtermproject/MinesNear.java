6d1221a50a999972fcac75748a3620d594665bc2
VERush <Vicki@rushworld.com>
2018-05-10 16:37:03 -0400

    Logic for edges added

--- src/midtermproject/MinesNear.java
+++ src/midtermproject/MinesNear.java
@@ -4,23 +4,53 @@ public class MinesNear {
 
 	public int calculateMinesNear(boolean mines[][], int x, int y) {
 		int minesNear = 0;
-		if (mines[x][y]) {
-			// Oh no! They hit a mine!
-			minesNear = 9;
+		int rows = mines.length;
+		int columns = mines[0].length;
+		int xBegin, xEnd, yBegin, yEnd;
+		
+		// Determine if we're on an edge and set indices so we don't go outside the mines array
+		if (x == 0) {
+			xBegin = 0;
 		} else {
-			// Cycle through rows nearby
-			for (int i = x - 1; i <= x + 1; i++) {
-				// Cycle through columns nearby
-				for (int j = y - 1; j <= y + 1; j++) {
-					// Don't count the cell we're in
-					if (i != j) {
-						// if there's a mine, count it
-						if (mines[i][j]) {
-							minesNear++;
+			xBegin = x - 1;
+		}	
+		if (x == mines.length) {
+			xEnd = mines.length;
+		} else {
+			xEnd = x + 1;
+		}
+			
+		if (y == 0) {
+			yBegin = 0;
+		} else {
+			yBegin = y - 1;
+		}	
+		if (y == mines.length) {
+			yEnd = mines.length;
+		} else {
+			yEnd = y + 1;
+		}
+		
+			if (mines[x][y]) {
+				// Oh no! They hit a mine!
+				minesNear = 9;
+			} else {
+				// Cycle through rows nearby
+				for (int i = xBegin; i <= xEnd; i++) {
+					// Cycle through columns nearby
+					for (int j = yBegin; j <= yEnd; j++) {
+						// Don't count the cell we're in
+						if (i != j) {
+							// if there's a mine, count it
+							if (mines[i][j]) {
+								minesNear++;
+							}
 						}
 					}
 				}
 			}
+		} else {
+
 		}
 		return minesNear;
 	}
