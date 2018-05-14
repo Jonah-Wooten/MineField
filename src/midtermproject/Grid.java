982debdcecd3c2dbc7b86435b75388d00ec7f3f0
VERush <Vicki@rushworld.com>
2018-05-14 11:08:55 -0400

    Deleted secret test grid

--- src/midtermproject/Grid.java
+++ src/midtermproject/Grid.java
@@ -12,18 +12,6 @@ import java.util.Collections;
  */
 public class Grid {
 
-	// hard coated test
-	public final static boolean[][] secret =
-
-			{ { false, false, false }, { false, true, true }, { true, true, false } };
-
-	public final static boolean[][] secret2 = { { true, true, true, false }, { true, false, true, false },
-			{ false, true, true, false }, { false, true, false, true } };
-
-	public final static boolean[][] secret3 = { { false, false, true, false, false }, { true, true, false, true, true },
-			{ true, false, true, false, true }, { false, true, false, true, false },
-			{ true, true, true, false, false } };
-
 	public boolean[][] randomGrid;
 	public int mineCount;
 
